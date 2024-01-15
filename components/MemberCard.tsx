a8fc80efb053dbcfc5e19a71f2273dd15c877f24
realToddler <emilien.jolivet@gmail.com>
2024-01-15 13:54:22 +0100

    fix

--- components/MemberCard.tsx
+++ components/MemberCard.tsx
@@ -3,14 +3,21 @@ import Image from "next/image";
 const MemberCard = (obj : {lastName : string, firstName : string, desc: string}) => {
   return (
     <>
-      <div className="md:w-1/6 w-4/12 bg-rosequartz rounded-lg p-4 space-y-2 text-justify border border-tekhelet">
-        <Image src={`/${obj.firstName.toLowerCase()}.png`} alt="self" width={500} height={500} 
-        className="rounded h-96"/>
-        <h1 className="text-lg text-russianviolet">{obj.lastName.toUpperCase()} {obj.firstName}</h1>
-        <div className="text-base">
-            {obj.desc}
+      <div className="md:w-64 h-1/2 bg-rosequartz rounded-lg p-4 space-y-2 text-justify border border-tekhelet flex">
+        <div className="h-full w-full">
+          <Image
+            src={`/${obj.firstName.toLowerCase()}.png`}
+            alt="self"
+            width={500}
+            height={500}
+            className="rounded h-5/6 w-full"
+          />
+          <h1 className="text-lg text-russianviolet">
+            {obj.lastName.toUpperCase()} {obj.firstName}
+          </h1>
+          <div className="text-base">{obj.desc}</div>
         </div>
-      </div> 
+      </div>
     </>
   );
 };

881dcda73ab0f85c22b7547a490de3012817e63d
realToddler <emilien.jolivet@gmail.com>
2024-01-16 15:42:39 +0100

    rounded pic

--- components/MemberCard.tsx
+++ components/MemberCard.tsx
@@ -1,19 +1,21 @@
 import Image from "next/image";
 
-const MemberCard = (obj : {lastName : string, firstName : string, desc: string}) => {
+const MemberCard = (obj: {
+  lastName: string;
+  firstName: string;
+  desc: string;
+}) => {
   return (
     <>
-      <div className="md:w-max-64 w-fit max-h-1/3 h-2/3 bg-rosequartz rounded-lg p-4 space-y-2 text-justify border border-tekhelet flex">
+      <div className="md:w-1/6 w-fit max-h-1/3 h-auto bg-rosequartz rounded-lg p-4 space-y-2 text-justify border border-tekhelet flex">
         <div className="h-full w-full">
-          <div className="h-4/5 w-full">
-            <Image
-              src={`/${obj.firstName.toLowerCase()}.png`}
-              alt="self"
-              width={500}
-              height={500}
-              className="rounded h-full w-full"
-            />
-          </div>
+          <Image
+            src={`/${obj.firstName.toLowerCase()}.png`}
+            alt="self"
+            width={500}
+            height={500}
+            className="rounded-full border-russianviolet border-2 h-full w-full"
+          />
           <h1 className="text-lg text-russianviolet">
             {obj.lastName.toUpperCase()} {obj.firstName}
           </h1>
--- pages/index.tsx
+++ pages/index.tsx
@@ -60,7 +60,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="md:min-h-screen md:h-fit" id="team">
+      <div className="md:min-h-screen md:h-fit text-white" id="team">
         <div className="flex text-xl justify-center bg-tekhelet items-center pb-8 min-h-auto">
           <div className="space-y-12">
             <div className="text-5xl underline pt-8">L{"'"}équipe</div>
@@ -76,7 +76,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="flex justify-center bg-timberwolf w-full" id="team">
+        <div className="flex justify-center bg-timberwolf w-full py-8" id="team">
           <div
             className="py-8 space-y-8 md:items-center
         md:py-0 md:space-y-0 md:flex md:justify-center md:space-x-12 md:w-full

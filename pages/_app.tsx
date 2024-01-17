7d873624ccdb6e904c544ef477069463bf13644c
realToddler <emilien.jolivet@gmail.com>
2024-01-17 15:17:52 +0100

    done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,50 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { DefaultSeo } from "next-seo";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const title = "GalaxyTraveller | By Cygnus";
+  const description = "GalaxyTraveller, a game made by Cygnus.";
+  return (
+    <>
+      <DefaultSeo
+        title={title}
+        description={description}
+        robotsProps={{
+          nosnippet: false,
+          notranslate: false,
+          noimageindex: false,
+          noarchive: false,
+          maxSnippet: -1,
+          maxImagePreview: "none",
+          maxVideoPreview: -1,
+        }}
+        openGraph={{
+          title,
+          locale: "fr",
+          type: "website",
+          url: "https://www.galaxytraveller.fr/",
+          images: [
+            {
+              url: "/logo_white.png",
+            },
+          ],
+          description,
+        }}
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "/logo_white.png",
+          },
+        ]}
+        additionalMetaTags={[
+          {
+            name: "robots",
+            content: "index, follow",
+          },
+        ]}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }

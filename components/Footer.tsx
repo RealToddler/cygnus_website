dc833761449a371d36f09012204aa67a65b73fa0
realToddler <emilien.jolivet@gmail.com>
2024-01-17 14:43:32 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <>
       <div className="bg-tekhelet">
-        <div className="md:h-fit h-28 p-4 text-white text-center text-sm">© 2023 Cygnus. All Rights Reserved.</div>
+        <div className="h-fit p-2 md:p-4 text-white text-center text-xs md:text-sm">© 2023 Cygnus. All Rights Reserved.</div>
       </div>
     </>
   );
